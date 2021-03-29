ad74f878e67f97d65ea4d9fd7636fc839ab21a79
jaskukka <oskari.kansanen@tuni.fi>
2021-03-29 10:27:53 +0300

    finish exercises 1.1 and 1.2

---
+++ osa1/kurssitiedot/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.first + props.second + props.third}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
+      <Total first={exercises1} second={exercises2} third={exercises3} />
+    </div>
+  )
+}
+
+export default App
